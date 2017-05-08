adebc8393124e385ff9de847ee1ba12ba61d730c
Tom Swann <ts.swann@gmail.com>
2017-05-08 14:54:37 +0100

    Fixed entry point structure

--- run.py
+++ run.py
@@ -5,4 +5,9 @@ Run main ETL application process
 import wmt_etl.etl_config as config
 
 
-print "Running load to {}".format(config.DB_SCHEMA)
+def main():
+    '''Main application entry point'''
+    print "Running load to {}".format(config.DB_SCHEMA)
+
+if __name__ == '__main__':
+    main()
